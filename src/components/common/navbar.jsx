e22c824de0d0b536f770a27a20fceb5fdcd82a6e
navya.giridharji <navya.giridharji@gmail.com>
2025-10-20 22:17:30 +0530

    Adding Registration component

---
+++ src/components/common/navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <AppBar position="static">
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Typography variant="h6" component="div">
+          Book Nest
+        </Typography>
+
+        <Box>
+          <Button
+            color="inherit"
+            component={RouterLink}
+            to="/register"
+            sx={{ mr: 2 }}
+          >
+            Register
+          </Button>
+          <Button
+            color="inherit"
+            component={RouterLink}
+            to="/login"
+          >
+            Login
+          </Button>
+
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
